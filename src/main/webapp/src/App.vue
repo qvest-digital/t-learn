b0c353b235feb4f9153e0177d5a234276684ab24
Christoph Büttgen <c.buettgen@tarent.de>
2020-08-21 14:21:03 +0200

    include Bootstrap and add Bootstrap components to CourseCreationForm

--- src/main/webapp/src/App.vue
+++ src/main/webapp/src/App.vue
@@ -1,6 +1,33 @@
 <template>
-  <div id="app">
-    <CourseCreationForm/>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <b-container fluid>
+        <a class="navbar-brand py-0" href="/">
+          <!--img src="/static/logo.webp" class="pr-2 " style="height:2rem;"-->
+          Weiterbildungsbörse
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="/foor">bar</a>
+            </li>
+          </ul>
+        </div>
+      </b-container>
+    </nav>
+    <b-container class="pt-5">
+      <CourseCreationForm></CourseCreationForm>
+    </b-container>
+    <footer class="container-fluid bg-light text-center mt-4">
+			<span class="text-muted">
+				something |
+				<a class="link-secondary" href="/privacy-policy">Privacy Policy</a>
+				| <a class="link-secondary" href="/impressum">Impressum</a>
+			</span>
+    </footer>
   </div>
 </template>
 
@@ -16,12 +43,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
