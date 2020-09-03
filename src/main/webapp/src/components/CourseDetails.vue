7bb40224c3e68d79014bfc30a94bbdb31ae77023
Christoph Büttgen <c.buettgen@tarent.de>
2020-09-03 10:53:01 +0200

    add details view

---
+++ src/main/webapp/src/components/CourseDetails.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <div>
+      <h1>{{ course.title }}</h1>
+    </div>
+    <div>
+      <div class="detailThumbnail">
+        <b-img left thumbnail class="mb-3 mr-3" :src="cardimage(course)"></b-img>
+      </div>
+      <b-row class="mb-3" cols="1" cols-md="2">
+        <b-col v-if="course.courseType">
+          <div class="label">Typ:</div>{{ course.courseType=='EXTERNAL' ? 'Extern' : 'Intern' }}
+        </b-col>
+        <b-col v-if="course.location">
+          <div class="label">Ort:</div>{{ course.location=='REMOTE' ? 'Remoteveranstaltung' : 'Präsenzveranstaltung' }}
+        </b-col>
+        <b-col v-if="course.startDate">
+          <div class="label">Start:</div>{{ course.startDate | formatDate }}
+        </b-col>
+        <b-col v-if="course.endDate">
+          <div class="label">Ende:</div>{{ course.endDate | formatDate }}
+        </b-col>
+        <b-col v-if="course.address">
+          <div class="label">Adresse:</div>{{ course.address }}
+        </b-col>
+        <b-col v-if="course.organizer">
+          <div class="label">Organisator:</div>{{ course.organizer }}
+        </b-col>
+        <b-col v-if="course.trainer">
+          <div class="label">Trainer:</div>{{ course.trainer }}
+        </b-col>
+        <b-col v-if="course.link">
+          <div class="label">Link:</div><div class="abbreviation"><a :href="course.link">{{ course.link }}</a></div>
+        </b-col>
+      </b-row>
+      <p>{{ course.targetAudience }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import coffeeImg from '../assets/coffee.jpg';
+import signsImg from '../assets/signs.jpg';
+
+export default {
+  name: "CourseDetails",
+  data: function () {
+    return {
+      course: {}
+    }
+  },
+  props: {
+    courseId: {
+      type: Number,
+      default: 0
+    }
+  },
+  watch: {
+    courseId: function (courseId) {
+      this.loadCourse(courseId)
+    }
+  },
+  methods: {
+    cardimage: function (course) {
+      switch (course.courseType) {
+        case "EXTERNAL":
+          return signsImg;
+        case "INTERNAL":
+        default:
+          return coffeeImg;
+      }
+    },
+    loadCourse: function (courseId) {
+      // TODO change hostname later
+      axios.get(`http://localhost:8080/courses/${courseId}`)
+          .then((response) => {
+            this.course = response.data;
+          })
+          .catch(() => this.course = {});
+    }
+  },
+  mounted: function () {
+    this.loadCourse(this.courseId);
+  }
+}
+</script>
+
+<style scoped>
+  .label {
+    width: 7rem;
+    font-weight: 700 !important;
+    float: left !important;
+  }
+
+  .detailThumbnail {
+    max-width: 20rem;
+  }
+
+  .abbreviation {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    width: calc(100% - 7rem);
+  }
+</style>
