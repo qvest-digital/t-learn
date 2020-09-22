3227baddc16b87424f1477f458c72f8cd7693dde
Mohammed Al-Sammarraie <m.al-sammarraie@tarent.de>
2020-09-22 16:18:41 +0200

    removed footer from app removed bootstrap from header.vue

--- src/main/webapp/src/App.vue
+++ src/main/webapp/src/App.vue
@@ -4,17 +4,15 @@
         <div class="app-container">
             <router-view></router-view>
         </div>
-        <Footer />
     </div>
 </template>
 
 <script>
 import Header from './components/Header';
-import Footer from './components/Footer';
 
 export default {
     name: 'App',
-    components: { Header, Footer }
+    components: { Header }
 };
 </script>
 
