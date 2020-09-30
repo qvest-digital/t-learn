fd93c9e44a3628d35646dba29625ad3429ed77e0
Mohammed Al-Sammarraie <m.al-sammarraie@tarent.de>
2020-09-30 13:52:46 +0200

    test for multipleSelect as todo

---
+++ src/main/webapp/tests/unit/components/MultipleSelect.spec.js
@@ -0,0 +1,39 @@
+import MultipleSelect from '@/components/MultipleSelect';
+import '@testing-library/jest-dom';
+import { fireEvent, render } from '@testing-library/vue';
+import Vue from 'vue';
+import vSelect from 'vue-select';
+
+Vue.component('v-select', vSelect);
+
+describe('MultipleSelect.vue', () => {
+    //TODO
+    test('check if input event is emitted on type', async () => {
+        // const { findByRole, getByRole, emitted } = setupComponent();
+        // const inputfield = getByRole('searchbox');
+        // await fireEvent.update(inputfield, 'f'.repeat(5));
+        // expect(emitted().input.length).toEqual(5);
+    });
+
+    function setupComponent() {
+        return render(
+            MultipleSelect,
+            {
+                propsData: {
+                    selectedCategories: [],
+                    categories: [
+                        'fontend',
+                        'backend',
+                        'css',
+                        'javascript',
+                        'vue',
+                        'java'
+                    ]
+                }
+            },
+            localVue => {
+                localVue.component('v-select', vSelect);
+            }
+        );
+    }
+});
