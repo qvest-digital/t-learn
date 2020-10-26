b3b16fd8e4c67a2b56a028db535c38d6536fe1f5
Christoph Büttgen <c.buettgen@tarent.de>
2020-10-26 13:50:51 +0100

    created an empty footer component
    minor CSS changes

--- src/main/webapp/src/App.vue
+++ src/main/webapp/src/App.vue
@@ -4,15 +4,17 @@
         <div class="app-container">
             <router-view></router-view>
         </div>
+        <Footer />
     </div>
 </template>
 
 <script>
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default {
     name: 'App',
-    components: { Header }
+    components: { Header, Footer }
 };
 </script>
 
