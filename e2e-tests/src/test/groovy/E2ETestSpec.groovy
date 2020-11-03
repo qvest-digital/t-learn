04af949c18da9380e111f2aa9b7d1d9984a7659f
Jan Schmitt <j.schmitt@tarent.de>
2020-11-03 13:00:06 +0100

    Add Tests for overview page

--- e2e-tests/src/test/groovy/E2ETestSpec.groovy
+++ e2e-tests/src/test/groovy/E2ETestSpec.groovy
@@ -6,10 +6,10 @@ import spock.lang.Shared
 @Testcontainers
 class E2ETestSpec extends GebSpec {
 
-    private static final String serviceName = "app-backend"
+    private static final String serviceName = 'app-backend'
     private static final int servicePort = 8080
 
-    DockerComposeContainer application = new DockerComposeContainer(new File("./e2e-docker-compose.yml"))
+    DockerComposeContainer application = new DockerComposeContainer(new File('./e2e-docker-compose.yml'))
             .withExposedService(serviceName, servicePort)
             .withLocalCompose(true);
 
